4d4a607c372e0ff9d438d4592812d911a595d8ae
armando980410 <81638107+armando980410@users.noreply.github.com>
2022-10-20 14:26:18 -0500

    Add files via upload

---
+++ Javascript/jueves20.js
@@ -0,0 +1,21 @@
+/*window.alert('Hola Mundo');
+
+var resultado= window.confirm('Estas seguro de la operación');
+
+if (resultado==true){
+    window.alert('verificamos tu decisión')
+}else{
+    window.alert('No verificamos tu decisión')
+};
+*/
+
+/*var folio= prompt('Escribe tu Folio Pilares: ');
+
+window.alert('Tu folio Pilares es: ' + folio)
+*/
+
+var folio= prompt('Escribe tu Folio Pilares: ');
+
+function button(){
+    return window.alert('Tu folio Pilares es: ' + folio);
+}
